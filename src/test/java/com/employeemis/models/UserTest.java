2c4e4e9463481a1675926d7f6e67a92de0f095a6
Steve <sntakirutimana72@gmail.com>
2025-04-17 20:18:14 +0200

    Added test unit placeholder for User model

--- src/test/java/com/employeemis/models/UserTest.java
+++ src/test/java/com/employeemis/models/UserTest.java
@@ -3,10 +3,11 @@ package com.employeemis.models;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserTest {
 
   @Test
   void setPassword() {
-
+    fail();
   }
 }
